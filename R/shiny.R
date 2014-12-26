f0025dbdcdb15fbad30d18c951ac53f5913dbcad
malexan <alexander@matrunich.com>
2014-12-26 12:36:23 +0300

    Experimental shiny barchart() added

---
+++ R/shiny.R
@@ -0,0 +1,54 @@
+#' Function to run an interactive Shiny-app to explore differences between 
+#' FCL and HS datasets.
+#' 
+#' @import shiny
+#' @import dplyr
+#' @import ggvis
+#' @export
+
+barchart <- function() {
+  shinyApp(
+    ui = fluidPage(
+      sidebarLayout(
+        sidebarPanel(sliderInput("w", "Bin width", 1, 20, 10),
+                     sliderInput("year", "Year", 2007, 2011, 2011,
+                                 format = "####"),
+                     selectInput("reporter", "Reporter", 
+                                 c("Mexico" = 138)),
+                     selectInput("pt", "Partner", 
+                                 c("USA" = 231)),
+                     selectInput("vrbl", "Quantity/Value",
+                                 c("Quantity (tonnes)" = "qdiff",
+                                   "Value ($1000)" = "vdiff")),
+                     sliderInput("rws", "Groups to display",
+                                 1, 100, 50)),
+        mainPanel(ggvisOutput("hist1"))
+      )
+    ), 
+    server = function(input, output) {
+      data <- reactive(read.table(system.file("extdata", 
+                                            "mxc_2011_2007_all_all.csv.gz", 
+                                            package = "fclhs"), 
+                                header = T,
+                                sep = ',', stringsAsFactors = F,
+                                nrows = 63582 # drop final line with timestamp
+      ) %>%
+        convertfao3(area = input$reporter, 
+                    partner = input$pt, 
+                    year = input$year) %>%
+        joincompared(getunct(area = unctarea(input$reporter),
+                             year = input$year,
+                             partner = unctarea(input$reporter), 
+                             compact = F) %>%
+                       convertunctapi()) %>% 
+        arrange_(paste0("desc(", input$vrbl, ")")) %>%
+        top_n(input$rws)
+      )
+
+      reactive({faithful %>% ggvis(~waiting)  %>%
+        layer_histograms(width = input$w) }) %>%
+        bind_shiny("hist1")
+    }
+  )
+  
+}
