1ce7cbc404a77325c1a1d72787abc01c35f5c0df
Alexander Matrunich <alexander@matrunich.com>
2015-02-09 18:48:18 +0300

    Random forest experiments

--- R/sws.R
+++ R/sws.R
@@ -13,9 +13,11 @@ getswsct <- function(area = NULL,
                      swshost = "hqlqasws1.hq.un.fao.org",
                      swsport = "8181",
                      swspath = "/sws",
-                     sessionid = "3870d0bb-2a46-4525-ba44-d9d246ecf502"
+                     sessionid = "3870d0bb-2a46-4525-ba44-d9d246ecf502",
+                     debug = F,
+                     testcodes = F
                      #, code = "AG6" #, compact = T,
-                     #desc = F, debug = F
+                     #desc = F
                      ) {
   if(!ping(swshost)) startjnc()
   
@@ -33,10 +35,8 @@ getswsct <- function(area = NULL,
   elevar <- "measuredElementTrade"
   yrvar  <- "timePointYears"
   elesfx <- c("00", "08", "21") # kg, units, US$
-  import <- "56"
-  export <- "59"
-
-  # TODO replace this !missing & is.ch. It leads to error if missing.
+  import <- "56" # Import
+  export <- "59" # Export
   
   ascharnotnullarg <- function(arg) {
     if(!is.null(arg) & !is.character(arg)) arg <- as.character(arg)
@@ -48,9 +48,15 @@ getswsct <- function(area = NULL,
   year    <- ascharnotnullarg(year)
   item    <- ascharnotnullarg(item)
   
+  
+  # TODO: wrap by function
   if(is.null(partner)) {
     partner <- GetCodeList(dmn, dtset, ptvar)$code
+  } else if(testcodes) {
+    if(sum(!(partner %in% GetCodeList(dmn, dtset, ptvar)$code)) > 0) 
+      stop("Provided code of partner not in DB")
   }
+
   
   if(is.null(item)) {
     item <- GetCodeList(dmn, dtset, itmvar)$code
@@ -97,6 +103,7 @@ getswsct <- function(area = NULL,
 #' if gets a response.
 #' 
 #' @source \url{ttps://github.com/mkao006/r_style_fao/blob/master/functions/sws_query.r}
+#' @export
 
 ping <- function(x, stderr = FALSE, stdout = FALSE, ...){
   
@@ -115,6 +122,7 @@ ping <- function(x, stderr = FALSE, stdout = FALSE, ...){
 #' 
 #' Juniper VPN software has to be installed.
 #' 
+#' @export
 startjnc <- function(path = "/usr/local/bin/", profile = "fao") {
   if(.Platform$OS.type != "unix") stop("Please set up a VPN connection with FAO intranet manually")
   system(paste0(path, "jnc --nox ", profile))
