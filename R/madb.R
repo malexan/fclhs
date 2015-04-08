ae37688283901e4c5226db063b9c24e1d07e69c3
Alexander Matrunich <alexander@matrunich.com>
2015-04-08 10:28:21 +0200

    Working version of getmadb()

--- R/madb.R
+++ R/madb.R
@@ -22,11 +22,41 @@ getmadbcountries <- function(url = "http://madb.europa.eu/madb/euTariffs.htm") {
 #' 
 #' @param country ISO3166 country code.
 #' @param hs HS numeric code of length 4 or 6.
+#' @param license.accept User has to explicitly accept the license.
+#'  FALSE by default.
+#' @param drop.mfn Should a most favoured nation clause be dropped
+#'  from resulting data.frame. TRUE by default.
 #' @return Dataframe with columns country, hs code and description.
 
-getmadb <- function(country, hs) {
+getmadb <- function(country, hs, license.accept = F, drop.mfn = T) {
   
+  if(!license.accept) stop("You have to accept Mendel Verlag licence.")
   
+  url <- "http://madb.europa.eu/madb/atDutyOverviewPubli.htm"
+  
+  r <- httr::POST(url, body = 
+                    list(datacat_id = "AT",
+                         hscode = as.character(hs),
+                         keyword = "",
+                         countries = toupper(country),
+                         datasettype = "",
+                         submit = "Search",
+                         showregimes = "",
+                         license = "Accept"),
+                  encode = "form")
+  
+  df <- XML::readHTMLTable(httr::content(r), which = 1, stringsAsFactors = F)
+  row.names(df) <- NULL
+  
+  df <- df[-1,] # First row contains NA only
+  colnames(df) <- c("hs", "desc", "mfn") # R-friendly colanames
+  if(drop.mfn) df <- df[,c("hs", "desc")] # Drop MFN if required
+  df <- cbind(country = toupper(country), df, stringsAsFactors = F) # Add country column
+  df$hs <- sapply(df$hs, function(x) { 
+    if(x == "") return(NA)
+    stringr::str_replace(x, "\\.", "")
+  })
+  
+  df
 }
-
 
