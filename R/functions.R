2dc359edf04817dac073b413499cd3872208df49
Alexander Matrunich <alexander@matrunich.com>
2015-05-07 14:50:21 +0200

    Forgot to save functions.R

--- R/functions.R
+++ R/functions.R
@@ -76,21 +76,6 @@ fcl2hs <- function(fcl) {
   unique(hsfclmap$hs[hsfclmap$fcl %in% fcl])
 }
 
-#' Description of FCL-item
-#' 
-#' @param item Numeric or character vector of FCL-codes. Character vector will 
-#' be converted into numeric.
-#' @return Character vector with descriptions
-#' 
-#' @export
-descfcl <- function(item) {
-  if(is.character(item)) item <- as.numeric(item)
-  if(length(item) > 1) return(unlist(lapply(item, descfcl)))
-  desc <- fclhs::fcl[fclhs::fcl$fcl == item, "fcltitle"]
-  if(length(desc) == 0) desc <- NA
-  desc
-}
-
 # It doesn't work correclty!!!
 reltype <- function(d) {
   d <- d %>%
